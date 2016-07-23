904b833432f1207e774144e9073eba71067472e5
vinod <vinod_nagaraj@apple.com>
2016-07-23 10:34:21 -0700

    changes done

--- ReactInputList/src/js/components/Input.js
+++ ReactInputList/src/js/components/Input.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ListItems from "./ListItems";
+import objectPath from "object-path";
 export default class Input extends React.Component{
   constructor(props){
     super(props);
@@ -9,20 +10,36 @@ export default class Input extends React.Component{
        InputList:[]
     }
   }
-  deleteItem(event){
-      console.log("delete this item");
+  _removeElement(eachvalue,index){
+    console.log("remove value::"+ eachvalue+"at index::" +index+" item to be deleted" +this);
+    return eachvalue!== this;
   }
-  handleClick(){ 
+  deleteItem(value){
+      console.log("delete this item" +value);
+      //array.filter(function(currentValue,index,arr), thisValue);
+      var pendingItem = this.state.InputList.filter(this._removeElement,value);
+      this.setState({
+        InputList : pendingItem
+      });
+  }
+  handleClick(){
       let inputList = this.state.InputList.concat(this.refs.input.value);
       this.setState({
         InputList : inputList
       });
       console.log("input"+this.state.InputList);
+      this.refs.input.value = "";
 
   }
   render(){
+    var obj = {
+    b: "d",
+    c: ["e", "f"],
+    '\u1200': 'unicode key',
+    'dot.dot': 'key'
+};
     console.log("rendering input");
-      
+    objectPath.set(obj,["a.h"], "m");
     return(
       <div>
         <input type="text" ref="input"/>
--- ReactInputList/src/js/components/ListItems.js
+++ ReactInputList/src/js/components/ListItems.js
@@ -4,15 +4,14 @@ import React from 'react';
 export default class ListItems extends React.Component{
 	constructor(props){
 		super(props);
-		this.handleClick = this.handleClick.bind(this);
 	}
-	handleClick(event){
-		this.props.deleteItem(event);
+	handleClick(value){
+		this.props.deleteItem(value);
 	}
 	render(){
 		var itemsArray = [];
 		this.props.items.map(function(value,index){
-			itemsArray.push(<li key={index}>{value} <button onClick={this.handleClick}>Delete</button></li>);
+			itemsArray.push(<li key={index}>{value} <button onClick={this.handleClick.bind(this,value)}>Delete</button></li>);
 		},this);
 
 
